2448f89a860c16f75fbb168ad2add8974badf9ee
ytakano2 <y.takano@lull-inc.co.jp>
2026-03-22 15:26:47 +0900

    新規登録途中セーブ

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
   <form action="{{ route('registerPost') }}" method="POST">
-    <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
-      <div class="w-25 vh-75 border p-3">
+    <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center; background:#ECF1F6">
+      <div class="w-25 vh-75 border p-3 bg-white custom-radius shadow">
         <div class="register_form">
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
