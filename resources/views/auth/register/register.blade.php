cab6fa49f2fe28c6c411ca332d0d3538c0866df9
ytakano2 <y.takano@lull-inc.co.jp>
2026-03-22 15:55:39 +0900

    新規登録途中セーブ

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
   <form action="{{ route('registerPost') }}" method="POST">
     <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center; background:#ECF1F6">
-      <div class="w-25 vh-75 border p-3 bg-white custom-radius shadow">
+      <div class="w-25 vh-75 border p-5 bg-white custom-radius shadow">
         <div class="register_form">
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
@@ -63,13 +63,19 @@
             @enderror
           </div>
         </div>
-        <div class="mt-3">
-          <input type="radio" name="sex" class="sex" value="1">
-          <label style="font-size:13px">男性</label>
-          <input type="radio" name="sex" class="sex" value="2">
-          <label style="font-size:13px">女性</label>
-          <input type="radio" name="sex" class="sex" value="3">
-          <label style="font-size:13px">その他</label>
+        <div class="mt-3 d-flex justify-content-around">
+          <div class="form-check">
+            <input type="radio" name="sex" class="sex" value="1">
+            <label style="font-size:13px">男性</label>
+          </div>
+          <div class="form-check">
+            <input type="radio" name="sex" class="sex" value="2">
+            <label style="font-size:13px">女性</label>
+          </div>
+          <div class="form-check">
+            <input type="radio" name="sex" class="sex" value="3">
+            <label style="font-size:13px">その他</label>
+          </div>
           @error('sex')
           <div class="text-danger">
             {{ $message }}
@@ -78,35 +84,22 @@
         </div>
         <div class="mt-3">
           <label class="d-block m-0 aa" style="font-size:13px">生年月日</label>
-          <select class="old_year" name="old_year">
-            <option value="none">-----</option>
-            <option value="1985">1985</option>
-            <option value="1986">1986</option>
-            <option value="1987">1987</option>
-            <option value="1988">1988</option>
-            <option value="1989">1989</option>
-            <option value="1990">1990</option>
-            <option value="1991">1991</option>
-            <option value="1992">1992</option>
-            <option value="1993">1993</option>
-            <option value="1994">1994</option>
-            <option value="1995">1995</option>
-            <option value="1996">1996</option>
-            <option value="1997">1997</option>
-            <option value="1998">1998</option>
-            <option value="1999">1999</option>
-            <option value="2000">2000</option>
-            <option value="2001">2001</option>
-            <option value="2002">2002</option>
-            <option value="2003">2003</option>
-            <option value="2004">2004</option>
-            <option value="2005">2005</option>
-            <option value="2006">2006</option>
-            <option value="2007">2007</option>
-            <option value="2008">2008</option>
-            <option value="2009">2009</option>
-            <option value="2010">2010</option>
-          </select>
+          <div class="border-bottom border-primary" style="width:50px;">
+            <select class="old_year border-0" name="old_year">
+              <option value="none">-----</option>
+              <option value="2000">2000</option>
+              <option value="2001">2001</option>
+              <option value="2002">2002</option>
+              <option value="2003">2003</option>
+              <option value="2004">2004</option>
+              <option value="2005">2005</option>
+              <option value="2006">2006</option>
+              <option value="2007">2007</option>
+              <option value="2008">2008</option>
+              <option value="2009">2009</option>
+              <option value="2010">2010</option>
+            </select>
+          </div>
           <label style="font-size:13px">年</label>
           <select class="old_month" name="old_month">
             <option value="none">-----</option>
