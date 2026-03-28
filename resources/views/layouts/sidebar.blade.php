ffc771a495e9d28b9c7364e3ed2a9943078cc9c9
ytakano2 <y.takano@lull-inc.co.jp>
2026-03-28 13:58:37 +0900

    サイドバーレイアウトOK

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -23,15 +23,25 @@
 <body class="all_content">
     <div class="d-flex">
         <div class="sidebar">
-            <p><a href="{{ route('top.show') }}"><i class="fa-regular fa-house" style="color: rgb(255, 255, 255);"></i> トップ</a></p>
-            <p><a href="/logout"><i class="fa-solid fa-arrow-right-from-bracket" style="color: rgb(255, 255, 255);"></i> ログアウト</a></p>
-            <p><a href="{{ route('calendar.general.show', ['user_id' => Auth::id()]) }}"><i class="fa-regular fa-calendar-days" style="color: rgb(255, 255, 255);"></i> スクール予約</a></p>
-            @can('admin')
-            <p><a href="{{ route('calendar.admin.show', ['user_id' => Auth::id()]) }}"><i class="fa-regular fa-calendar-check" style="color: rgb(255, 255, 255);"></i> スクール予約確認</a></p>
-            <p><a href="{{ route('calendar.admin.setting', ['user_id' => Auth::id()]) }}"><i class="fa-regular fa-calendar-plus" style="color: rgb(255, 255, 255);"></i> スクール枠登録</a></p>
-            @endcan
-            <p><a href="{{ route('post.show') }}"><i class="fa-solid fa-comment-dots" style="color: rgb(255, 255, 255);"></i> 掲示板</a></p>
-            <p><a href="{{ route('user.show') }}"><i class="fa-solid fa-user-group" style="color: rgb(255, 255, 255);"></i> ユーザー検索</a></p>
+            <div class="mt-4 ml-2">
+                <p><a href="{{ route('top.show') }}"><i class="fa-regular fa-house"
+                            style="color: rgb(255, 255, 255);"></i> トップ</a></p>
+                <p><a href="/logout"><i class="fa-solid fa-arrow-right-from-bracket"
+                            style="color: rgb(255, 255, 255);"></i> ログアウト</a></p>
+                <p><a href="{{ route('calendar.general.show', ['user_id' => Auth::id()]) }}"><i
+                            class="fa-regular fa-calendar-days" style="color: rgb(255, 255, 255);"></i> スクール予約</a></p>
+                @can('admin')
+                    <p><a href="{{ route('calendar.admin.show', ['user_id' => Auth::id()]) }}"><i
+                                class="fa-regular fa-calendar-check" style="color: rgb(255, 255, 255);"></i> スクール予約確認</a>
+                    </p>
+                    <p><a href="{{ route('calendar.admin.setting', ['user_id' => Auth::id()]) }}"><i
+                                class="fa-regular fa-calendar-plus" style="color: rgb(255, 255, 255);"></i> スクール枠登録</a></p>
+                @endcan
+                <p><a href="{{ route('post.show') }}"><i class="fa-solid fa-comment-dots"
+                            style="color: rgb(255, 255, 255);"></i> 掲示板</a></p>
+                <p><a href="{{ route('user.show') }}"><i class="fa-solid fa-user-group"
+                            style="color: rgb(255, 255, 255);"></i> ユーザー検索</a></p>
+            </div>
         </div>
         <div class="main-container">
             {{ $slot }}
