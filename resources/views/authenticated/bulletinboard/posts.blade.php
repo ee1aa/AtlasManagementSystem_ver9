2fe015227ddab544525b7f915c4edda15f93494d
ytakano2 <y.takano@lull-inc.co.jp>
2026-03-28 16:56:58 +0900

    投稿一覧検索ボタンOK

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -34,16 +34,17 @@
                 </div>
             @endforeach
         </div>
-        <div class="other_area border w-25">
-            <div class="border m-4">
-                <div class=""><a href="{{ route('post.input') }}" class="btn btn_search_post searches">投稿</a>
+        <div class="other_area w-25">
+            <div class="m-4">
+                <div class=""><a href="{{ route('post.input') }}"
+                        class="btn btn_search_post searches mb-3">投稿</a>
                 </div>
-                <div class="d-inline-flex btn-group searches">
+                <div class="d-inline-flex btn-group searches mb-3">
                     <input type="text" placeholder="キーワードを検索" name="keyword" form="postSearchRequest"
                         class="input_keyword">
                     <input type="submit" value="検索" form="postSearchRequest" class="btn btn_search_post">
                 </div>
-                <div class="d-inline-flex searches">
+                <div class="d-flex searches mb-3">
                     <input type="submit" name="like_posts" class="category_btn plum p-2" value="いいねした投稿"
                         form="postSearchRequest">
                     <input type="submit" name="my_posts" class="category_btn orange p-2" value="自分の投稿"
